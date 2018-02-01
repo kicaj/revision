5a55319f620b3b3be0786321b473b1c357a70655
kicaj <kicaj@kdev.pl>
2018-02-01 21:44:54 +0100

    First version

---
+++ src/Model/Behavior/RevisionBehavior.php
@@ -0,0 +1,82 @@
+<?php
+namespace Revision\Model\Behavior;
+
+use Cake\Datasource\EntityInterface;
+use Cake\ORM\Behavior;
+use Cake\Database\Query;
+use Cake\Database\Expression\Comparison;
+use Cake\Event\Event;
+use Cake\Utility\Text;
+use Cake\Database\Connection;
+use Cake\Datasource\ConnectionManager;
+
+class RevisionBehavior extends Behavior
+{
+
+    /**
+     * Default config
+     *
+     * @var array
+     */
+    protected $_defaultConfig = [
+        'prefix' => 'revision_',
+        'field' => 'hash'
+    ];
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeSave(Event $event, EntityInterface $entity)
+    {
+        $hash = mb_substr(Text::uuid(), 9, 12);
+
+        $revision = $this->_table->newEntity();
+
+        $revision = $this->_table->find()->where([
+            $this->_table->getPrimaryKey() => $entity->{$this->_table->getPrimaryKey()}
+        ])->first();
+
+        if ($entity->isNew()) {
+            if (!isset($entity->{$this->_config['prefix'] . $this->_config['field']})) {
+                $entity->{$this->_config['prefix'] . $this->_config['field']} = $hash;
+            }
+        } else {
+            $revision->{$this->_config['prefix'] . $this->_table->getPrimaryKey()} = $entity->{$this->_table->getPrimaryKey()};
+
+            $revision->unsetProperty($this->_table->getPrimaryKey())->isNew(true);
+
+            $this->_table->save($revision);
+
+            // Disable foreign keys
+            $this->_table->connection()->disableForeignKeys();
+
+            $entity->{$this->_config['prefix'] . $this->_config['field']} = $hash;
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function beforeFind(Event $event, Query $query)
+    {
+        if ($this->_table->hasField($this->_config['prefix'] . $this->_config['field'])) {
+            $revisioned = true;
+
+            if ($query->clause('where')) {
+                $query->clause('where')->traverse(function ($expression) use (&$revisioned) {
+                    if ($expression instanceof Comparison) {
+                        if ($expression->getField() === $this->_table->getAlias() . '.' . $this->_config['prefix'] . $this->_config['field']) {
+                            $revisioned = false;
+                        }
+                    }
+                });
+            }
+
+            if ($revisioned === true) {
+                $query->where($this->_table->getAlias() . '.' . $this->_config['prefix'] . $this->_table->getPrimaryKey() . ' IS NULL');
+            }
+        }
+
+        return $query;
+    }
+}
